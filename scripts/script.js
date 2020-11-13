48301632dc334099ef032846c1d1f73b714cc9e9
maximkorobeinik <72419327+maximkorobeinik@users.noreply.github.com>
2020-11-13 22:56:03 +0300

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,13 @@
-// $(document).ready(function(){
-//     $('.header_images').slick({
-//     });
-//   });
+$(document).ready(function(){
+    $('.mob_slides').slick({
+        arrows: false
+    });
+});
+$(document).ready(function(){
+    $('.desktop_slides').slick({
+        arrows: false,
+        autoplay: true,
+        autoplaySpeed: 5000,
+        speed: 1500
+    });
+});
